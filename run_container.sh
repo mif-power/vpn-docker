7b0f9315950bb2e9d5f931b4f6727437149e81a2
mif-power <p0we7man@gmail.com>
2025-11-23 01:07:32 +0300

    Port param added to start script

--- run_container.sh
+++ run_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 clear
-docker run -ti --network=bridge -v ~/vpn:/vpn -p 8080:8080 --privileged --rm cka3o4nik/vpn-proxy:latest bash /vpn/container_start_squid.sh "$@"
+docker run -ti --network=bridge -v ~/vpn:/vpn -p $1:8080 --privileged --rm cka3o4nik/vpn-proxy:latest bash /vpn/container_start_squid.sh $2
