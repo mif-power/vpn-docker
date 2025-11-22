9175387cd3cbe482c36ed7bb2291e108ff21b3a9
mif-power <p0we7man@gmail.com>
2025-11-23 01:55:00 +0300

    Added OpenVPN AS container config. Container data folder changed

--- run_AS_container.sh
+++ run_AS_container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -d --name=openvpn-as --device /dev/net/tun --cap-add=MKNOD --cap-add=NET_ADMIN -p 943:943 -p 443:443 -p 1194:1194/udp -v /etc/openvpn-as:/openvpn --restart=unless-stopped  openvpn/openvpn-as
+cp -R openvpn-as-config /openvpn-as
+docker run -d --name=openvpn-as --device /dev/net/tun --cap-add=MKNOD --cap-add=NET_ADMIN -p 943:943 -p 443:443 -p 1194:1194/udp -v /openvpn-as:/openvpn --restart=unless-stopped  openvpn/openvpn-as
